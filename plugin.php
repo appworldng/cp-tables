6a83ceb5e563331582c963ab6f968e322723e6b6
chigozieorunta <chigozieorunta@yahoo.com>
2020-07-08 10:16:44 +0100

    Added plugin file

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Custom Post Tables
+ * Description: A simple plugin to help you display custom posts in a table like fashion.
+ * Plugin URI:  https://github.com/chigozieorunta
+ * Version:     1.0.0
+ * Author:      Chigozie Orunta
+ * Author URI:  https://github.com/chigozieorunta
+ * Text Domain: cptables
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) )
+	require __DIR__ . '/vendor/autoload.php';
+
+//Use CustomPostsTables\Plugin as cptables;
+
+//CustomPostsTables\Plugin::getInstance();
+
